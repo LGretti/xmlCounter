8b83901bd31b364a3fc1bcd406660eed5cdfa786
Lucas Gretti <lucassgretti@gmail.com>
2022-06-26 23:55:04 -0300

    Funcionalidade de seleção de múiltiplos arquivos XML

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,46 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.ofdXML = new System.Windows.Forms.OpenFileDialog();
             this.btnSelecionar = new System.Windows.Forms.Button();
-            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.edtTag = new System.Windows.Forms.TextBox();
-            this.fbdXML = new System.Windows.Forms.FolderBrowserDialog();
+            this.dlgXML = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dlgXMLArq = new System.Windows.Forms.OpenFileDialog();
+            this.txtArqs = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
-            // 
-            // ofdXML
-            // 
-            this.ofdXML.FileName = "Selecione a fonte de dados";
-            this.ofdXML.Filter = "Arquivos XML (*.xml)|*.xml";
-            this.ofdXML.Multiselect = true;
-            this.ofdXML.Title = "Selecione os arquivos XML";
             // 
             // btnSelecionar
             // 
-            this.btnSelecionar.Location = new System.Drawing.Point(57, 78);
+            this.btnSelecionar.Location = new System.Drawing.Point(44, 103);
             this.btnSelecionar.Name = "btnSelecionar";
-            this.btnSelecionar.Size = new System.Drawing.Size(89, 23);
+            this.btnSelecionar.Size = new System.Drawing.Size(91, 46);
             this.btnSelecionar.TabIndex = 0;
-            this.btnSelecionar.Text = "Selecionar...";
+            this.btnSelecionar.Text = "Selecionar diretório...";
             this.btnSelecionar.UseVisualStyleBackColor = true;
             this.btnSelecionar.Click += new System.EventHandler(this.button1_Click);
             // 
             // edtTag
             // 
-            this.edtTag.Location = new System.Drawing.Point(45, 49);
+            this.edtTag.Location = new System.Drawing.Point(44, 66);
             this.edtTag.Name = "edtTag";
-            this.edtTag.Size = new System.Drawing.Size(110, 23);
+            this.edtTag.Size = new System.Drawing.Size(91, 23);
             this.edtTag.TabIndex = 1;
             // 
-            // fbdXML
+            // dlgXML
             // 
-            this.fbdXML.InitialDirectory = "C:\\";
+            this.dlgXML.InitialDirectory = "C:\\";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Informe o nome da tag";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "<";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(135, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = ">";
+            // 
+            // txtArqs
+            // 
+            this.txtArqs.Enabled = false;
+            this.txtArqs.FormattingEnabled = true;
+            this.txtArqs.ItemHeight = 15;
+            this.txtArqs.Location = new System.Drawing.Point(193, 12);
+            this.txtArqs.Name = "txtArqs";
+            this.txtArqs.Size = new System.Drawing.Size(595, 409);
+            this.txtArqs.TabIndex = 5;
             // 
             // frmContador
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 434);
+            this.Controls.Add(this.txtArqs);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.edtTag);
             this.Controls.Add(this.btnSelecionar);
             this.Name = "frmContador";
@@ -79,11 +116,13 @@
         }
 
         #endregion
-
-        private OpenFileDialog ofdXML;
         private Button btnSelecionar;
-        private FolderBrowserDialog folderBrowserDialog1;
         private TextBox edtTag;
-        private FolderBrowserDialog fbdXML;
+        private FolderBrowserDialog dlgXML;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private OpenFileDialog dlgXMLArq;
+        private ListBox txtArqs;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+using System.Xml;
+
 namespace xmlCounter
 {
     public partial class frmContador : Form
@@ -9,7 +11,22 @@ namespace xmlCounter
 
         private void button1_Click(object sender, EventArgs e)
         {
-            fbdXML.ShowDialog(this);
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = true;
+            ofd.Filter = "Arquivos XML (*.xml)|*.xml";
+            ofd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string filepath in ofd.FileNames)
+                {
+                    txtArqs.Items.Add(filepath);
+
+                    XmlTextReader xmlReader = new XmlTextReader(filepath);
+
+                }
+            }
+
         }
 
         private void frmContador_Load(object sender, EventArgs e)
